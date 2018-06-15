d3fdff2d3c033426f9fe702788bbeb384eac2166
maskwang520 <826655812@qq.com>
2018-06-15 21:45:33 +0800

    fuyong

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/netty/NettyComponent.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/netty/NettyComponent.java
@@ -23,7 +23,7 @@ public class NettyComponent implements ApplicationContextAware, InitializingBean
 
     private void consumerServerStart(int port) {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup(5);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(6);
         try {
             ServerBootstrap b = new ServerBootstrap().group(bossGroup, workerGroup);
             b.channel(NioServerSocketChannel.class)
@@ -42,7 +42,7 @@ public class NettyComponent implements ApplicationContextAware, InitializingBean
 
     private void providerServerStart(int port) {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup(3);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(4);
         try {
             ServerBootstrap sbs = new ServerBootstrap().group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
